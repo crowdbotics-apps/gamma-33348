5a707079f6694b5f058f0ee3796ba2e5ea45779e
Crowdbotics <team@crowdbotics.com>
2022-02-04 04:36:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gamma_33348/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gamma_33348;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gamma_33348";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gamma_33348",
+  url: "https://gamma_33348.botics.co",
+  api: "https://gamma_33348.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
